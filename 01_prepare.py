6d1fa0f8ec694a8cbedb7fe7e7ee5993d89c1811
Ronie Uliana <ronie.uliana@gmail.com>
2016-04-18 17:11:44 -0300

    Quase pronto, agora é só rodar com o conjunto final.
    O código está um lixo. Mas funcionar primeiro e depois deixar bacana. :)

--- 01_prepare.py
+++ 01_prepare.py
@@ -2,6 +2,7 @@
 # Remove records and labels that are less representative
 import json
 import codecs
+from collections import Counter
 
 INPUT_FILE = 'vagas_e_cargos.json'
 OUTPUT_FILE = 'vagas_e_cargos_01.json'
@@ -11,23 +12,67 @@ MIN_PEOPLE = 50
 # Mininum percent of people in a label (job description)
 MIN_PERC_LABEL = 0.05
 
+qtde_pre = 0
+counter_pre = Counter()
+unique_pre = set()
+
+qtde_pos = 0
+counter_pos = Counter()
+unique_pos = set()
+
 with codecs.open(INPUT_FILE, 'r', encoding='utf-8') as file_in:
     with codecs.open(OUTPUT_FILE, 'w', encoding='utf-8') as file_out:
         for line in file_in:
             record = json.loads(line)
-            if not record.has_key(u'cargos'):
-                continue
+            if not record.has_key(u'cargos'): continue
 
             title = record[u'cargo_vaga']
             text = record[u'AnuncioWeb_vaga']
             labels = [x for x in record[u'cargos'] if x[0] != None and x[0].strip() != '']
 
+            qtde_pre += 1
+            #if qtde_pre > 10: break
+            lbls = frozenset([text for text, number in labels])
+            counter_pre[lbls] += 1
+            unique_pre.update(lbls)
+
             labels_sum = float(sum([x[1] for x in labels]))
-            if labels_sum < MIN_PEOPLE:
-                continue
+            if labels_sum < MIN_PEOPLE: continue
 
             labels = [x for x in labels if x[1] / labels_sum > MIN_PERC_LABEL]
-            if len(labels) == 0:
-                continue
+            if len(labels) == 0: continue
+
+            qtde_pos += 1
+            lbls = frozenset([text for text, number in labels])
+            counter_pos[lbls] += 1
+            unique_pos.update(lbls)
 
             file_out.write(json.dumps([title, text, labels]) + '\n')
+
+print('Antes da limpeza')
+ulabl_pre = len(unique_pre)
+label_pre = sum([len(s) * q for s, q in counter_pre.most_common()])
+diver_pre = len(counter_pre)
+divnr_pre = diver_pre / float(qtde_pre)
+lcard_pre = label_pre / float(qtde_pre)
+ldens_pre = lcard_pre / float(qtde_pre)
+print('    Registros: %d' % qtde_pre)
+print('      Rótulos: %d' % ulabl_pre)
+print('  Diversidade: %d' % diver_pre)
+print('Divers. Norm.: %0.2f' % divnr_pre)
+print('Cardinalidade: %0.2f' % lcard_pre)
+print('    Densidade: %0.8f' % ldens_pre)
+
+print('Após a limpeza')
+ulabl_pos = len(unique_pos)
+label_pos = sum([len(s) * q for s, q in counter_pos.most_common()])
+diver_pos = len(counter_pos)
+divnr_pos = diver_pos / float(qtde_pos)
+lcard_pos = label_pos / float(qtde_pos)
+ldens_pos = lcard_pos / float(qtde_pos)
+print('    Registros: %d' % qtde_pos)
+print('      Rótulos: %d' % ulabl_pos)
+print('  Diversidade: %d' % diver_pos)
+print('Divers. Norm.: %0.2f' % divnr_pos)
+print('Cardinalidade: %0.2f' % lcard_pos)
+print('    Densidade: %0.8f' % ldens_pos)
--- 05_predict_by_copy.py
+++ 05_predict_by_copy.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import numpy as np
 from random import sample
 from load_data import load_data_raw
@@ -71,41 +75,77 @@ class MultiLabelEmsemble(BaseEstimator):
         return self
 
     def predict(self, X):
-        rslts = [None] * len(X)
+        rslts = [Counter() for _ in X]
         for target, clf in self.clf_bag:
             ps = clf.predict(X)
             for i, p in enumerate(ps):
                 if p == 1:
-                    rslt = Counter() if rslts[i] == None else rslts[i]
-                    rslts[i] = rslt
-                    rslt.update(target)
+                    rslts[i].update(target)
         return rslts
 
-full_set = load_data_raw('data/amostra_01perc_4.json')
+full_set = load_data_raw('data/amostra_10perc_6.json')
 
 X = np.array([title for identity, title, text, label in full_set])
 y = np.array([frozenset(label) for identity, title, text, label in full_set])
 
-targets = [u'estagio', u'vendedor']
+targets = [u'civil engenheiro', u'vendedor', u'enfermeiro', u'arquiteto']
 fs = [frozenset([i for i, labels in enumerate(y) if t in labels]) for t in targets]
 z = list(reduce(lambda x, y: x | y, fs))
 X = X[z]
 y = y[z]
 
-#for text, label in zip(X[:30], y):
-#    print('[%s] %s => %s' % (', '.join(label), text, tokenize(text)))
+clfs = []
+for n in range(10):
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    pipeline = Pipeline(steps=[('vectorizer', tfidf_vectorizer()),
+                               ('classifier', SGDClassifier(loss='hinge', class_weight='balanced'))])
 
-pipeline = Pipeline(steps=[('vectorizer', tfidf_vectorizer()),
-                           ('classifier', SGDClassifier(loss='hinge'))])
+    clf = MultiLabelEmsemble(clf=pipeline)
+    clf.fit(x_train, y_train)
+    clfs.append(clf)
 
-clf = MultiLabelEmsemble(clf=pipeline)
-clf.fit(x_train, y_train)
-preds = clf.predict(x_test)
+preds = [clf.predict(x_test) for clf in clfs]
+preds = [reduce(lambda x, y: x + y, ps) for ps in zip(*preds)]
 
-for i, predicted in enumerate(preds):
-    sample = x_test[i]
-    real = ', '.join(y_test[i])
-    print('%s => %s | %s' % (sample, real, predicted))
+for n in range(10, 20):
+    print('=' * 60)
+    print('Threshold: %d' % n)
+
+    preds1 = [frozenset([t for t, c in p.most_common() if c > n]) for p in preds]
+
+    precs = []
+    recs = []
+    f1s = []
+    for sample, real, predicted in zip(x_test, y_test, preds1):
+        real = frozenset(real)
+        tp = float(len(real & predicted))
+        prec = tp / len(predicted) if len(predicted) > 0 else 0
+        rec = tp / len(real) if len(real) > 0 else 0
+        f1 = 2 * ((prec * rec) / (prec + rec)) if prec + rec > 0 else 0
+        real_str = '[%s]' % ', '.join(real)
+        predicted_str =  '[%s]' % ', '.join(predicted)
+        # print('[prec: %0.2f, rec: %0.2f, f1: %0.2f] %s => %s | %s' % (prec, rec, f1, sample, real_str, predicted_str))
+        print('%0.3f\t%0.3f\t%0.3f' % (prec, rec, f1))
+        precs.append(prec)
+        recs.append(rec)
+        f1s.append(f1)
+
+    print('-' * 60)
+    print('Precisão: %0.2f' % (sum(precs) / len(precs)))
+    print('1ºQ Precisão: %0.2f' % (np.percentile(precs, 25)))
+    print('Med Precisão: %0.2f' % (np.median(precs)))
+    print('3ºQ Precisão: %0.2f' % (np.percentile(precs, 75)))
+
+    print('-' * 60)
+    print('Abrangência: %0.2f' % (sum(recs) / len(recs)))
+    print('1ºQ Abrangência: %0.2f' % (np.percentile(recs, 25)))
+    print('Med Abrangência: %0.2f' % (np.median(recs)))
+    print('3ºQ Abrangência: %0.2f' % (np.percentile(recs, 75)))
+
+    print('-' * 60)
+    print('F1 Score: %0.2f' % (sum(f1s) / len(f1s)))
+    print('1ºQ F1 Score: %0.2f' % (np.percentile(f1s, 25)))
+    print('Med F1 Score: %0.2f' % (np.median(f1s)))
+    print('3ºQ F1 Score: %0.2f' % (np.percentile(f1s, 75)))
 
